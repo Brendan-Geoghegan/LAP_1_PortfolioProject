6dfd745fd1a773455f14cb777c6033d4e5b7bd3b
Brendan Geoghegan <brendangeoghegan@outlook.com>
2022-10-04 11:26:29 +0100

    first three tests added

--- test/app.test.js
+++ test/app.test.js
@@ -15,6 +15,26 @@ describe('API server', () => {
         api.close(done);
     })
 
+    it('responds to get / with a status of 200 and says hello', (done) => {
+        request(api).get('/').expect("Hello").expect(200, done);
+    })
 
-    
+    it('retrieves all entries', (done) => {
+        request(api).get('/entries').expect(200, done)
+    })
+
+    it('retrieves a specific entry', (done) => {
+        request(api).get('/entries/0').expect(200)
+        .expect({
+            id: 0,
+            entry: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptatibus fuga harum quibusdam, culpa dolorem odio id omnis excepturi. Aspernatur eligendi, unde facere impedit iusto possimus tempora placeat doloremque dolores.",
+            comments: [],
+            reactions: {
+                smiley: 1202,
+                sad: 13,
+                like: 200
+            },
+            gif: ""
+        }, done)
+    })
 })
